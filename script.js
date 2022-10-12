efbc12563f3eb307f031c1aa73682bf4c448ddc2
Stud.net <st.codding@gmail.com>
2022-10-12 23:08:27 +0300

    project is completed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict"; 
+
+const section = document.querySelector('section'),
+		overlay = document.querySelector('.overlay'),
+		showModal = document.querySelector('.show-modal'),
+		closeBtn = document.querySelector('.close-btn');
+
+		showModal.addEventListener('click', ()=> section.classList.add('active'));
+		overlay.addEventListener('click', ()=> section.classList.remove('active'));
+
+		closeBtn.addEventListener('click', ()=> section.classList.remove('active'));
